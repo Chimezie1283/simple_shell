843cd56aca252037750d495557c6b24554e8e7fb
Chimezie1283 <agbo.chimezie1283@gmail.com>
2023-07-26 02:22:51 -0700

    updated by chi

---
+++ chi_error1.c
@@ -0,0 +1,72 @@
+#include "errors.h"
+
+char *errors_127(char **args);
+char *errors_128(char **args);
+
+/**
+ * errors_127 - This creates message for permission denied.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *errors_127(char **args)
+{
+	char *error, *hist_str;
+	int lent;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	lent = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 24;
+	error = malloc(sizeof(char) * (lent + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": ");
+	_strcat(error, args[0]);
+	_strcat(error, ": Permission declined\n");
+
+	free(hist_str);
+	return (error);
+}
+
+/**
+ * errors_128 - This creates message for command not found.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *errors_128(char **args)
+{
+	char *error, *hist_str;
+	int lent;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	lent = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 16;
+	error = malloc(sizeof(char) * (lent + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": ");
+	_strcat(error, args[0]);
+	_strcat(error, ": Request not found\n");
+
+	free(hist_str);
+	return (error);
+}
