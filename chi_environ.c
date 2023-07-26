6e7215e6513e3b2b49caf45e826ace983cd8a10a
Chimezie1283 <agbo.chimezie1283@gmail.com>
2023-07-26 01:27:52 -0700

    updated by chi

--- chi_environ.c
+++ chi_environ.c
@@ -18,15 +18,15 @@ int shellto_unsetenv(char **args, char __attribute__((__unused__)) **vront);
 int shellto_env(char **args, char __attribute__((__unused__)) **vront)
 {
 	int indez;
-	char nc = '\n';
+	char yc = '\n';
 
 	if (!environ)
 		return (-1);
 
 	for (indez = 0; environ[indez]; indez++)
 	{
-		write(STDOUT_FILENO, environ[indez], _stringlen(environ[indez]));
-		write(STDOUT_FILENO, &nc, 1);
+		write(STDOUT_FILENO, environ[indez], _strlen(environ[indez]));
+		write(STDOUT_FILENO, &yc, 1);
 	}
 
 	(void)args;
@@ -45,43 +45,43 @@ int shellto_env(char **args, char __attribute__((__unused__)) **vront)
  */
 int shellto_setenv(char **args, char __attribute__((__unused__)) **vront)
 {
-	char **env_var = NULL, **new_environ, *new_value;
+	char **env_var = NULL, **new_env, *new_val;
 	size_t size;
 	int indez;
 
 	if (!args[0] || !args[1])
 		return (create_error(args, -1));
 
-	new_value = malloc(_stringlen(args[0]) + 1 + _stringlen(args[1]) + 1);
-	if (!new_value)
+	new_val = malloc(_strlen(args[0]) + 1 + _strlen(args[1]) + 1);
+	if (!new_val)
 		return (create_error(args, -1));
-	_stringcpy(new_value, args[0]);
-	_stringcat(new_value, "=");
-	_stringcat(new_value, args[1]);
+	_strcpy(new_val, args[0]);
+	_strcat(new_val, "=");
+	_strcat(new_val, args[1]);
 
 	env_var = _getenv(args[0]);
 	if (env_var)
 	{
 		free(*env_var);
-		*env_var = new_value;
+		*env_var = new_val;
 		return (0);
 	}
 	for (size = 0; environ[size]; size++)
 		;
 
-	new_environ = malloc(sizeof(char *) * (size + 2));
-	if (!new_environ)
+	new_env = malloc(sizeof(char *) * (size + 2));
+	if (!new_env)
 	{
-		free(new_value);
+		free(new_val);
 		return (create_error(args, -1));
 	}
 
 	for (indez = 0; environ[indez]; indez++)
-		new_environ[indez] = environ[indez];
+		new_env[indez] = environ[indez];
 
 	free(environ);
-	environ = new_environ;
-	environ[indez] = new_value;
+	environ = new_env;
+	environ[indez] = new_val;
 	environ[indez + 1] = NULL;
 
 	return (0);
@@ -98,7 +98,7 @@ int shellto_setenv(char **args, char __attribute__((__unused__)) **vront)
  */
 int shellto_unsetenv(char **args, char __attribute__((__unused__)) **vront)
 {
-	char **env_var, **new_environ;
+	char **env_var, **new_env;
 	size_t size;
 	int indez, indez2;
 
@@ -111,8 +111,8 @@ int shellto_unsetenv(char **args, char __attribute__((__unused__)) **vront)
 	for (size = 0; environ[size]; size++)
 		;
 
-	new_environ = malloc(sizeof(char *) * size);
-	if (!new_environ)
+	new_env = malloc(sizeof(char *) * size);
+	if (!new_env)
 		return (create_error(args, -1));
 
 	for (indez = 0, indez2 = 0; environ[indez]; indez++)
@@ -122,11 +122,11 @@ int shellto_unsetenv(char **args, char __attribute__((__unused__)) **vront)
 			free(*env_var);
 			continue;
 		}
-		new_environ[indez2] = environ[indez];
+		new_env[indez2] = environ[indez];
 		indez2++;
 	}
 	free(environ);
-	environ = new_environ;
+	environ = new_env;
 	environ[size - 1] = NULL;
 
 	return (0);
